45f729ff8c67367ff7dc7cdbb64c3c03f9a36ffa
ps777 <56358194+piotrszubert@users.noreply.github.com>
2022-09-13 19:52:58 +0200

    moved nav, fixes, added active class to link

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<style>
+.offcanvas.offcanvas-end {
+    border: 0;
+}
+</style>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Offcanvas navbar large">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">acab</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Offcanvas</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link" class:active={$page.url.pathname === '/'} aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" class:active={$page.url.pathname === '/contact'} href="/contact">Kontakt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" class:active={$page.url.pathname === '/about'} href="/about">O nas</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
