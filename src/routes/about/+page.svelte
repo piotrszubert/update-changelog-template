a4f362136b0f1b688cc4c997a364dcf6adedf006
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-04-27 12:09:38 +0000

    re-initalize project

--- src/routes/about/+page.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-    <title>
-        About
-    </title>
-    <meta name="description" content="Acab o nas. Acab historia">
-</svelte:head>
-
-<h1>about</h1>
--- src/routes/contact/card.svelte
+++
@@ -1,8 +0,0 @@
-<div class="card" style="width: 18rem;">
-    <img src="https://images.pexels.com/photos/13415115/pexels-photo-13415115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#=/" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
