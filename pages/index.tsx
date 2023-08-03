04cfc670cd7358597a655719c0494bc790749b54
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-03 22:36:11 +0200

    disabled header, changed bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      {/* <Header/> */}
       <main className={`${nunito.className} ${styles.main}`}>
         <div>  
           <SubscribeForm/>
