e0d722c3b6f97d6b9a3bfb174a64959d790d7cda
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-09 21:34:40 +0200

    path names based on title

--- components/card.tsx
+++ components/card.tsx
@@ -57,12 +57,12 @@ export default function Card({title, releaseDate, newFeatures, improvements, bug
             </div>
 
             <div className={styles.cardFooter}>
-                <a href="version/version1">
+                {title && <a href={`/version/${title.replaceAll(' ', '-')}`}>
                     Read more
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                     </svg>
-                </a>
+                </a>}
             </div>
         </div>
     );
