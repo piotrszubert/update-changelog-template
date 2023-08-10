d431a7e67cc33dca0417fde53cd9812377148c1a
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-10 12:07:58 +0200

    added more content

--- data/versionsData.ts
+++ data/versionsData.ts
@@ -188,6 +188,198 @@ const data = [
 			},
 		],
 	},
+	
+	// Version 4.0.0
+    {
+        id: 'ver.4.0.0',
+        title: "Version 4.0.0",
+        releaseDate: "10-03-2023",
+        bgColor: "var(--card-bg-rgb-4)",
+        newFeatures: [
+            // New features for version 4.0.0
+            {
+                short: "Introducing AI-powered assistant",
+                long:
+                    "Introducing AI-powered assistant: Meet our new AI assistant that can help you manage tasks, set reminders, and provide personalized recommendations based on your usage patterns.",
+            },
+            // Add more new features for version 4.0.0 if needed
+            {
+                short: "Enhanced document collaboration",
+                long:
+                    "Enhanced document collaboration: Collaborate more effectively with your team by editing documents in real-time and tracking changes with an improved version history.",
+            },
+        ],
+        improvements: [
+            // Improvements for version 4.0.0
+            {
+                short: "Redesigned settings menu",
+                long:
+                    "Redesigned settings menu: We've revamped the settings menu for a cleaner and more organized layout, making it easier to customize your app preferences.",
+            },
+            // Add more improvements for version 4.0.0 if needed
+            {
+                short: "Optimized memory usage",
+                long:
+                    "Optimized memory usage: We've optimized the app's memory consumption, leading to improved performance and reduced resource usage.",
+            },
+        ],
+        bugFixes: [
+            // Bug fixes for version 4.0.0
+            {
+                short: "Fixed image sharing",
+                long:
+                    "Fixed image sharing: Users reported issues with sharing images from the app. We've resolved this problem, and image sharing now functions seamlessly.",
+            },
+            // Add more bug fixes for version 4.0.0 if needed
+            {
+                short: "Addressed notification delay",
+                long:
+                    "Addressed notification delay: Some users experienced delays in receiving notifications. This issue has been addressed, ensuring timely alerts.",
+            },
+        ],
+        otherUpdates: [
+            // Other updates for version 4.0.0
+            {
+                short: "Enhanced data privacy",
+                long:
+                    "Enhanced data privacy: We've implemented additional privacy measures to protect your personal information and ensure secure data handling.",
+            },
+            // Add more other updates for version 4.0.0 if needed
+            {
+                short: "Improved accessibility features",
+                long:
+                    "Improved accessibility features: We've made accessibility enhancements to ensure that the app is more usable and inclusive for all users.",
+            },
+        ],
+    },
+
+    // Version 4.1.0
+    {
+        id: 'ver.4.1.0',
+        title: "Version 4.1.0",
+        releaseDate: "25-06-2023",
+        bgColor: "var(--card-bg-rgb-5)",
+        newFeatures: [
+            // New features for version 4.1.0
+            {
+                short: "Multi-device sync",
+                long:
+                    "Multi-device sync: Seamlessly synchronize your data across multiple devices, allowing you to switch between devices while maintaining a consistent experience.",
+            },
+            // Add more new features for version 4.1.0 if needed
+            {
+                short: "Voice commands",
+                long:
+                    "Voice commands: Interact with the app using voice commands for hands-free control. Perform tasks, search, and navigate with voice recognition technology.",
+            },
+        ],
+        improvements: [
+            // Improvements for version 4.1.0
+            {
+                short: "Redesigned note-taking",
+                long:
+                    "Redesigned note-taking: We've completely revamped the note-taking feature, offering more formatting options, better organization, and improved usability.",
+            },
+            // Add more improvements for version 4.1.0 if needed
+            {
+                short: "Faster image loading",
+                long:
+                    "Faster image loading: Images now load quicker within the app, providing a smoother browsing experience when viewing visual content.",
+            },
+        ],
+        bugFixes: [
+            // Bug fixes for version 4.1.0
+            {
+                short: "Resolved sync conflicts",
+                long:
+                    "Resolved sync conflicts: Users experienced conflicts during data synchronization. We've fixed these issues, ensuring consistent and accurate syncing.",
+            },
+            // Add more bug fixes for version 4.1.0 if needed
+            {
+                short: "Fixed crash on note export",
+                long:
+                    "Fixed crash on note export: Some users encountered crashes when exporting notes. This issue has been resolved, and exporting notes is now stable.",
+            },
+        ],
+        otherUpdates: [
+            // Other updates for version 4.1.0
+            {
+                short: "Improved offline mode",
+                long:
+                    "Improved offline mode: Enjoy enhanced functionality while offline, including access to recently viewed items and the ability to edit and save changes for syncing later.",
+            },
+            // Add more other updates for version 4.1.0 if needed
+            {
+                short: "Expanded keyboard shortcuts",
+                long:
+                    "Expanded keyboard shortcuts: We've added more keyboard shortcuts to improve navigation and streamline common tasks for power users.",
+            },
+        ],
+    },
+
+    // Version 4.1.1
+    {
+        id: 'ver.4.1.1',
+        title: "Version 4.1.1",
+        releaseDate: "10-08-2023",
+        bgColor: "var(--card-bg-rgb-6)",
+        newFeatures: [
+            // New features for version 4.1.1
+            {
+                short: "Customizable themes",
+                long:
+                    "Customizable themes: Personalize your app experience with a wide range of themes and color options, allowing you to choose the look that suits you best.",
+            },
+            // Add more new features for version 4.1.1 if needed
+            {
+                short: "Quick access shortcuts",
+                long:
+                    "Quick access shortcuts: Easily access your favorite features and frequently used actions through customizable shortcuts for increased efficiency.",
+            },
+        ],
+        improvements: [
+            // Improvements for version 4.1.1
+            {
+                short: "Enhanced attachment handling",
+                long:
+                    "Enhanced attachment handling: Managing attachments is now smoother and more intuitive, with improved support for various file types and cloud services.",
+            },
+            // Add more improvements for version 4.1.1 if needed
+            {
+                short: "Smoother animations",
+                long:
+                    "Smoother animations: Enjoy smoother and more fluid animations throughout the app, enhancing the overall visual experience.",
+            },
+        ],
+        bugFixes: [
+            // Bug fixes for version 4.1.1
+            {
+                short: "Addressed text formatting bug",
+                long:
+                    "Addressed text formatting bug: Users encountered issues with text formatting inconsistencies. We've fixed this bug, ensuring consistent formatting in all instances.",
+            },
+            // Add more bug fixes for version 4.1.1 if needed
+            {
+                short: "Fixed occasional data loss",
+                long:
+                    "Fixed occasional data loss: Some users experienced data loss in specific scenarios. We've resolved this issue, preventing any further data loss occurrences.",
+            },
+        ],
+        otherUpdates: [
+            // Other updates for version 4.1.1
+            {
+                short: "Improved search suggestions",
+                long:
+                    "Improved search suggestions: The app now provides more accurate and contextually relevant search suggestions, helping you find what you need faster.",
+            },
+            // Add more other updates for version 4.1.1 if needed
+            {
+                short: "Updated user guides",
+                long:
+                    "Updated user guides: We've refreshed the user guides and documentation to provide clearer instructions and assistance for both new and existing users.",
+            },
+        ],
+    },
 	// Add more versions here if needed
 ];
 
