d1dd91dc821071dd14853940201b6e40b15301f9
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-11 20:52:40 +0200

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
 import { Nunito } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Header from '@/components/header'
 import SubscribeForm from '@/components/subscribeForm'
-import Card from '@/components/card'
 import CardList from '@/components/cardList'
 
 const nunito = Nunito({ subsets: ['latin'] })
@@ -17,7 +15,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Header/> */}
       <main className={`${nunito.className} ${styles.main}`}>
         <div>  
           <SubscribeForm/>
